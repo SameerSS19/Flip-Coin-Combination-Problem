2082e2a7d2d4c426139e0e2d7385b1c240851977
unknown <sam1919>
2021-05-02 23:32:17 +0530

    Add:Simulator loop create a doublet combination

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -7,14 +7,30 @@ read -p "Enter number of flip required " n
 for (( i=0; i<n; i++ ))
 do
 	result=$(( RANDOM%2 ))
-	if [ $result == 1 ]
+if [ $result == 1 ];
 	then
          	head=$(($head+1))
 	else
         	tail=$(($tail+1))
 	fi
+	sum=$(( RANDOM%4 ));
+	case $sum in
+		0) hh=$(($hh+1))
+			;;
+		1) ht=$(($ht+1))
+			;;
+		2) th=$(($th+1))
+			;;
+		3) tt=$(($tt+1))
+			;;
+		*) echo "Went  wrong"
+			;;
+	esac
 done
 
 echo "Number of head win" $head
-
 echo "Number of tail win" $tail
+echo "Number of hh combination " $hh
+echo "Number of ht combination " $ht
+echo "Number of th combination " $th
+echo "Number of tt combination " $tt
